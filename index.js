aa5af53f61e38fb8d547dc13900c8d64d8252241
BigTimeCoffee <padraigdowling1@hotmail.com>
2021-10-29 07:29:34 -0300

    project 3b backend

--- index.js
+++ index.js
@@ -1,91 +1,99 @@
-const express = require('express');
-const app = express();
-const data = require('./data');
-const crypto = require('crypto');
-const port = 1000;
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const data = require('./data')
+const crypto = require('crypto')
+const port = 1000
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
 app.engine(
     'hbs',
     handlebars({
         layoutsDir: `${__dirname}/views/layouts`, // layoutsDir is a property that points to the necessary folder/files
         extname: 'hbs', // changes the extension name from 'handlebars' to 'hbs'
     })
-);
-app.use(express.static('public')); //points to the public folder where index.html is stored
+)
+app.use(express.static('public')) //points to the public folder where index.html is stored
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.post('/users', (req, res) => {
+app.get('/', (req, res) => {
+    res.render('main', { layout: 'index' })
+}) // render has 2 peramaters, 'main' which points to the handlebars file and an object with the layout property pointing to the index file
+
+app.get('/users', (req, res) => {
+    let usersData = data.users
+    console.log(usersData)
+
+    res.render('users', { layout: 'index', usersData })
+})
+
+app.get('/schedules', (req, res) => {
+    let schedulesData = data.schedules
+    console.log(schedulesData)
+    res.render('schedules', { layout: 'index', schedulesData })
+})
+
+app.get(`/users/:userId/schedules`, (req, res) => {
+    let schedulesData = data.schedules.filter((obj) => {
+        return req.params.userId == obj.user_id
+    })
+    res.render('schedules', { layout: 'index', schedulesData })
+})
+
+app.get('/users/new', (req, res) => {
+    res.render('form', { layout: 'index' })
+})
+
+app.post('/users/new', (req, res) => {
     let hash = crypto
         .createHash('sha256')
         .update(req.body.password)
-        .digest('base64');
-    let newUser = {
+        .digest('base64')
+    const userDetails = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
         password: hash,
-    };
-    data.users.push(newUser);
-    res.send(newUser);
-});
+    }
+    data.users.push(userDetails)
+    res.redirect('/users/new')
+})
 
-app.get('/', (req, res) => {
-    res.render('main', { layout: 'index' });
-}); // render has 2 peramaters, 'main' which points to the handlebars file and an object with the layout property pointing to the index file
+app.get('/schedules/new', (req, res) => {
+    let usersData = data.users
+    console.log(usersData)
+    for (i = 0; i < usersData.length; i++) {
+        usersData[i].id = i
+    }
+    console.log('Users after for', usersData)
+    res.render('scheduleForm', { layout: 'index', usersData })
+})
 
-app.get('/users', (req, res) => {
-    let usersData = data.users;
-    console.log(usersData);
-    res.render('users', { layout: 'index' });
-});
+app.post('/schedules/new', (req, res) => {
+    const schedulesData = {
+        user_id: req.body.user_id,
+        day: req.body.day,
+        start_at: req.body.start_at,
+        end_at: req.body.end_at,
+    }
 
-app.get('/schedules', (req, res) => {
-    res.render('schedules', { layout: 'index' });
-});
+    data.schedules.push(schedulesData)
+
+    res.redirect('/schedules/new')
+})
 
 app.get('/users/:id', (req, res) => {
-    let id = req.params.id;
+    let id = req.params.id
     if (data.users[id]) {
-        let userData = data.users[id];
-        res.render('users', { layout: 'index', name });
+        let usersData = data.users[id]
+        res.render('user', { layout: 'index', usersData })
     } else {
-        res.json('Unknown');
+        res.json('Unknown')
     }
-});
+})
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
-
-//app.use(bodyParser.json());
-
-/*app.get('/', (req, res) => {
-    res.send('Welcome to our schedule website');
-});*/
-
-/*app.get('/users', (req, res) => {
-    res.send(data.users);
-});*/
-
-/*app.get('/schedules', (req, res) => {
-    res.send(data.schedules);
-});*/
-
-/*app.get('/users/:id', (req, res) => {
-    let id = req.params.id;
-    if (data.users[id]) {
-        res.json(data.users[id]);
-    } else {
-        res.json('Unknown');
-    }
-});*/
-
-/*app.get(`/users/:userId/schedules`, (req, res) => {
-    let newSchedules = data.schedules.filter((obj) => {
-        return req.params.userId == obj.user_id;
-    });
-    console.log(newSchedules);
-    res.send(newSchedules);
-});*/
+    console.log(`listening on port ${port}`)
+})
