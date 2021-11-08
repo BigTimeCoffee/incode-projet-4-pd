57dcdf142ee6686cb2d66648ec96328664064205
BigTimeCoffee <padraigdowling1@hotmail.com>
2021-11-08 08:34:54 -0300

    db working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const data = require('./data')
+//const data = require('./data')
 const crypto = require('crypto')
 const handlebars = require('express-handlebars')
+const bodyparser = require('body-parser')
 
-require('dotenv').config() // process.env.filename to access the variable
+require('dotenv').config() // process.env.var to access the variable.
 
 const password = process.env.PASSWORD
 
@@ -40,26 +41,35 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('main', { layout: 'index' })
-}) // render has 2 peramaters, 'main' which points to the handlebars file and an object with the layout property pointing to the index file
-
-app.get('/users', (req, res) => {
-    let usersData = data.users
-    console.log(usersData)
-
-    res.render('users', { layout: 'index', usersData })
 })
 
 app.get('/schedules', (req, res) => {
-    let schedulesData = data.schedules
-    console.log(schedulesData)
-    res.render('schedules', { layout: 'index', schedulesData })
+    connection.query('SELECT * FROM schedules', function (err, result) {
+        if (err) throw err
+        console.log(result)
+        res.render('schedules', { layout: 'index', result })
+    })
+})
+// render has 2 peramaters, 'main' which points to the handlebars file and an object with the layout property pointing to the index file
+
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM users', function (err, result) {
+        if (err) throw err
+        console.log(result)
+        res.render('users', { layout: 'index', result })
+    })
 })
 
-app.get(`/users/:userId/schedules`, (req, res) => {
-    let schedulesData = data.schedules.filter((obj) => {
-        return req.params.userId == obj.user_id
-    })
-    res.render('schedules', { layout: 'index', schedulesData })
+app.get(`/users/:id/schedules`, (req, res) => {
+    connection.query(
+        'SELECT * FROM schedules WHERE id = ?',
+        [req.params.id],
+        (err, result) => {
+            if (err) throw err
+            console.log(result)
+            res.render('schedulesId', { layout: 'index', result })
+        }
+    )
 })
 
 app.get('/users/new', (req, res) => {
@@ -72,46 +82,55 @@ app.post('/users/new', (req, res) => {
         .update(req.body.password)
         .digest('base64')
     const userDetails = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         email: req.body.email,
         password: hash,
     }
-    data.users.push(userDetails)
-    res.redirect('/users/new')
+    let dataInsert = 'INSERT INTO users SET ?'
+    connection.query(dataInsert, userDetails, function (err, result) {
+        if (err) throw err
+        console.log('User data is inserted successfully ')
+    })
+    res.redirect('/')
 })
 
 app.get('/schedules/new', (req, res) => {
-    let usersData = data.users
-    console.log(usersData)
-    //for (i = 0; i < usersData.length; i++) {
-    //    usersData[i].id = i
-
-    console.log('Users after for', usersData)
-    res.render('scheduleForm', { layout: 'index', usersData })
+    connection.query('SELECT first_name, id FROM users', (err, userData) => {
+        if (err) throw err
+        console.log('User schedule is inserted successfully ')
+        res.render('scheduleForm',  { layout: 'index', userData })
+        //console.log(userData)
 })
+    })
+    
 
 app.post('/schedules/new', (req, res) => {
     const schedulesData = {
         user_id: req.body.user_id,
         day: req.body.day,
-        start_at: req.body.start_at,
-        end_at: req.body.end_at,
+        start_time: req.body.start_time,
+        end_time: req.body.end_time,
     }
 
-    data.schedules.push(schedulesData)
-
-    res.redirect('/schedules/new')
+    let schedulesInsert = 'INSERT INTO schedules SET ?'
+    connection.query(schedulesInsert, schedulesData, function (err, result) {
+        if (err) throw err
+        console.log('User schedule is inserted successfully ')
+    })
+    res.redirect('/')
 })
 
 app.get('/users/:id', (req, res) => {
-    let id = req.params.id
-    if (data.users[id]) {
-        let usersData = data.users[id]
-        res.render('user', { layout: 'index', usersData })
-    } else {
-        res.json('Unknown')
-    }
+    connection.query(
+        'SELECT * FROM users WHERE id = ?',
+        [req.params.id],
+        (err, result) => {
+            if (err) throw err
+            console.log(result)
+            res.render('user', { layout: 'index', result })
+        }
+    )
 })
 
 app.listen(port, () => {
